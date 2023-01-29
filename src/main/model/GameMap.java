eec7b587dcdfb76b35bda32df2c9332bbfa156b9
ahasan02 <ahasan02@student.ubc.ca>
2023-01-29 11:31:52 -0800

    Added method to chisel basic circles (with boundary clamps) and tests

--- src/main/model/GameMap.java
+++ src/main/model/GameMap.java
@@ -1,5 +1,8 @@
 package model;
 
+import static java.lang.Math.*;  // we do a lot of math stuff here
+import static model.TileKind.*;  // just to make it less verbose
+
 public class GameMap {
     private final Tile[][] tiles;
     private final int[] shape;
@@ -12,7 +15,7 @@ public class GameMap {
         for (int i = 0; i < mapWidth; i++) {
             for (int j = 0; j < mapHeight; j++) {
                 try {
-                    this.tiles[i][j] = new Tile(TileKind.WALL);
+                    this.tiles[i][j] = new Tile(WALL);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -38,18 +41,42 @@ public class GameMap {
 
     // REQUIRES: this.shape[0] > x + w, this.shape[1] > y + h
     // MODIFIES: this.tiles[x][y]
-    // EFFECTS: Set a single tile to a new tile
+    // EFFECTS: Set a rectangle into floor tiles
     void chiselRectangle(int x, int y, int w, int h) throws Exception {
         for (int i = x; i < w+x; i++) {
             for (int j = y; j < h+y; j++) {
-                this.tiles[i][j] = new Tile(TileKind.FLOOR);
+                this.setTile(i,j, FLOOR);
             }
         }
     }
 
+    void chiselCircle(int x, int y, int r) throws Exception {
+        // based off equation of a circle:
+        // r^2 = (x-a)^2 + (y-b)^2
+
+        // clamps
+        int minX = max(x-r, 0);
+        int maxX = min(x+r, this.shape[0]);
+        int minY = max(y-r, 0);
+        int maxY = min(y+r, this.shape[1]);
+
+        // essentially, we will iterate over an rxr square
+        // we will then see if the area of the rect from that point to (x, y) is
+        // less than r^2, and if so we will chisel the area out
+        for (int i = minX; i < maxX; i++) {
+            for (int j = minY; j < maxY; j++) {
+                if (pow(r, 2) >= pow(abs(i-x), 2) + pow(abs(j-y),2)) {
+                    this.setTile(i,j,FLOOR);
+                }
+            }
+        }
+
+    }
+
+
     // REQUIRES: x < this.shape[0], y < this.shape[1]
     // MODIFIES: this.tiles[x][y]
-    // EFFECTS: Set a single tile to a new tile
+    // EFFECTS: Set a single tile to a new tile. Pretty wrapper
     public void setTile(int x, int y, TileKind kind) throws Exception {
         this.tiles[x][y] = new Tile(kind);
     }
--- src/test/model/TestGameMap.java
+++ src/test/model/TestGameMap.java
@@ -21,7 +21,7 @@ public class TestGameMap {
     }
 
     @Test
-    void createRoom() throws Exception {
+    void createRectRoom() throws Exception {
         assertFalse(rectMap.getTile(3, 3).isWalkable());
         rectMap.chiselRectangle(3,3,4,4);
         assertTrue(rectMap.getTile(3, 3).isWalkable());
@@ -30,6 +30,24 @@ public class TestGameMap {
         squareMap.chiselRectangle(1,1,SQUARE_LENGTH-2,SQUARE_LENGTH-2);
         assertTrue(squareMap.getTile(3, 3).isWalkable());
         assertTrue(squareMap.getTile(7, 10).isWalkable());
+    }
+
+    @Test
+    void createCircleRoom() throws Exception {
+        assertFalse(rectMap.getTile(8, 8).isWalkable());
+        rectMap.chiselCircle(9,9,5);
+        assertTrue(rectMap.getTile(8, 8).isWalkable());
+        // corner is still wall
+        assertFalse(rectMap.getTile(5, 5).isWalkable());
+    }
+
+    @Test
+    void circleChiselClamps() throws Exception {
+        assertFalse(rectMap.getTile(0, 0).isWalkable());
+        rectMap.chiselCircle(3,2,4);
+        assertTrue(rectMap.getTile(0, 0).isWalkable());
+        assertTrue(rectMap.getTile(0, 2).isWalkable());
+        assertTrue(rectMap.getTile(6, 2).isWalkable());
     }
 
 }

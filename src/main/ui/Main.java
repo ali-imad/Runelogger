8221112345db283fdc4ea94202b9c5f81ec942a1
ahasan02 <ahasan02@student.ubc.ca>
2023-02-26 18:48:43 -0500

    Commit before checkout

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,14 +1,12 @@
 package ui;
 
 import model.BossLog;
-import model.Item;
 
 public class Main {
-    // TEST
-    private static Item[] testLoot = new Item[]{new Item(0, 42031), new Item(1, 12378)};
-
     public static void main(String[] args) {
         BossLog log = new BossLog();
+
+        // TEST PREFILLS
         log.addNewEntry(0, 108, 42893);
         log.addNewEntry(2, 78, 18893);
         log.addNewEntry(1, 149, 52893);

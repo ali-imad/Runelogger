72036b7bd4858302d676a268ac06723717532678
ahasan02 <ahasan02@student.ubc.ca>
2023-02-27 00:23:03 -0500

    Added class level documentation for MenuOption

--- src/main/ui/MenuOption.java
+++ src/main/ui/MenuOption.java
@@ -1,18 +1,23 @@
 package ui;
 
+/*
+    A menu option for the UI to process and render
+ */
 public class MenuOption {
-    private final char button;
-    private final String description;
+    private final char button;  // button correspondng to this option
+    private final String description;  // label for the button
 
     public MenuOption(char button, String desc) {
         this.button = button;
         this.description = desc;
     }
 
+    // EFFECTS: Returns the options character for selection
     public char getButton() {
         return button;
     }
 
+    // EFFECTS: Returns the option label
     public String getDescription() {
         return description;
     }
